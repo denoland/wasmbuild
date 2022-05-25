93209b678398c3d645419d9301ae38f38ba995c1
crowlkats <crowlkats@toaxl.com>
2022-05-25 17:38:07 +0200

    add initial bindgen function

--- rs-lib/src/lib.rs
+++ rs-lib/src/lib.rs
@@ -1,12 +1,25 @@
+fn bindgen(wasm: &[u8]) -> Result<String, anyhow::Error> {
+  let x = wasm_bindgen_cli_support::Bindgen::new()
+    .deno(true)?
+    .weak_refs(true)
+    .input_module("foo", walrus::Module::from_buffer(wasm)?)
+    .generate_output()?;
+
+  println!("{}", x.js());
+
+  Ok(x.js().to_string())
+}
+
 
 #[cfg(test)]
 mod tests {
+  use crate::bindgen;
+
   // todo: temporary
   const TEST_WASM_BYTES: &[u8] = include_bytes!("../deno_test.wasm");
 
   #[test]
   fn it_works() {
-    let result = 2 + 2;
-    assert_eq!(result, 4);
+    std::fs::write("test.js", bindgen(TEST_WASM_BYTES).unwrap()).unwrap();
   }
 }

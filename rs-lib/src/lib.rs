1ace9ac5e3ac1f700c18f13d6b0920907f1fa531
David Sherret <dsherret@gmail.com>
2022-05-25 12:02:25 -0400

    More work.

--- rs-lib/src/lib.rs
+++ rs-lib/src/lib.rs
@@ -1,15 +1,31 @@
-fn bindgen(wasm: &[u8]) -> Result<String, anyhow::Error> {
-  let x = wasm_bindgen_cli_support::Bindgen::new()
+use wasm_bindgen::prelude::*;
+use anyhow::Result;
+
+#[derive(serde::Serialize)]
+pub struct Output {
+  pub js: String,
+  pub wasm: Vec<u8>,
+}
+
+#[wasm_bindgen]
+pub fn bindgen(wasm: &[u8]) -> Result<JsValue, JsValue> {
+  let output = inner(wasm).map_err(|err| JsValue::from(js_sys::Error::new(&err.to_string())))?;
+  let output = JsValue::from_serde(&output).map_err(|err| JsValue::from(js_sys::Error::new(&err.to_string())))?;
+  Ok(output)
+}
+
+fn inner(wasm: &[u8]) -> Result<Output> {
+  let mut x = wasm_bindgen_cli_support::Bindgen::new()
     .deno(true)?
     .weak_refs(true)
     .input_module("foo", walrus::Module::from_buffer(wasm)?)
     .generate_output()?;
 
-  println!("{}", x.js());
-
-  Ok(x.js().to_string())
+  Ok(Output {
+    js: x.js().to_string(),
+    wasm: x.wasm_mut().emit_wasm(),
+  })
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -20,6 +36,5 @@ mod tests {
 
   #[test]
   fn it_works() {
-    std::fs::write("test.js", bindgen(TEST_WASM_BYTES).unwrap()).unwrap();
   }
 }

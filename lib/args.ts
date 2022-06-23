0a81f5d23b6bbb42b5a545fd3f24f1824a8c7aac
David Sherret <dsherret@gmail.com>
2022-06-23 12:33:21 -0400

    Deprecate `--check` flag.

--- lib/args.ts
+++ lib/args.ts
@@ -28,55 +28,59 @@ export interface CheckCommand extends CommonBuild {
 
 export function parseArgs(rawArgs: string[]): Command {
   const flags = parseFlags(rawArgs);
-  let isCheck = flags.check ?? false;
+  if (flags.check) {
+    throw new Error(
+      `The --check flag has been removed. Use the 'check' subcommand.`,
+    );
+  }
+
   switch (flags._[0]) {
     case "new":
       return {
         kind: "new",
       };
     case "check":
-      isCheck = true;
-      break;
+      return {
+        kind: "check",
+        ...getCommonBuild(),
+      };
     case "build":
     case undefined:
     case null:
-      break;
+      return {
+        kind: "build",
+        ...getCommonBuild(),
+      };
     default:
       throw new Error(`Unrecognized sub command: ${flags._[0]}`);
   }
 
-  const cargoFlags = [];
-
-  if (flags["default-features"] === false) {
-    cargoFlags.push("--no-default-features");
-  }
-  if (flags["features"]) {
-    cargoFlags.push(`--features`);
-    cargoFlags.push(flags["features"]);
-  }
-  if (flags["all-features"]) {
-    cargoFlags.push("--all-features");
-  }
-
-  const base: CommonBuild = {
-    profile: flags.debug ? "debug" : "release",
-    project: flags.p ?? flags.project,
-    isSync: flags.sync ?? false,
-    isOpt: !(flags["skip-opt"] ?? false),
-    outDir: flags.out ?? "./lib",
-    bindingJsFileExt: flags["js-ext"] ?? `js`,
-    cargoFlags,
-  };
-
-  if (isCheck) {
+  function getCommonBuild(): CommonBuild {
     return {
-      kind: "check",
-      ...base,
+      profile: flags.debug ? "debug" : "release",
+      project: flags.p ?? flags.project,
+      isSync: flags.sync ?? false,
+      isOpt: !(flags["skip-opt"] ?? false),
+      outDir: flags.out ?? "./lib",
+      bindingJsFileExt: flags["js-ext"] ?? `js`,
+      cargoFlags: getCargoFlags(),
     };
-  } else {
-    return {
-      kind: "build",
-      ...base,
-    };
+  }
+
+  function getCargoFlags() {
+    const cargoFlags = [];
+
+    if (flags["default-features"] === false) {
+      cargoFlags.push("--no-default-features");
+    }
+    if (flags["features"]) {
+      cargoFlags.push(`--features`);
+      cargoFlags.push(flags["features"]);
+    }
+    if (flags["all-features"]) {
+      cargoFlags.push("--all-features");
+    }
+
+    return cargoFlags;
   }
 }

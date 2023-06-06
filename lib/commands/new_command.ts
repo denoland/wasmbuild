6da26a3464c723af9b3478b37455100c8a1c7b2a
Nbiba Bedis <bedisnbiba@gmail.com>
2023-06-06 20:22:08 +0100

    check available tools in new command

--- lib/commands/new_command.ts
+++ lib/commands/new_command.ts
@@ -5,6 +5,8 @@ import { versions } from "../versions.ts";
 import { pathExists } from "../helpers.ts";
 
 export async function runNewCommand() {
+  await checkIfRequiredToolsExist();
+
   if (await pathExists("./rs_lib")) {
     console.log(
       `${
@@ -115,4 +117,26 @@ async function getFileTextIfExists(path: string) {
       throw err;
     }
   }
+}
+
+async function checkIfRequiredToolsExist() {
+  const requiredTools = ["deno", "cargo", "rustup", "wasm-bindgen"];
+  const notInstalled: string[] = [];
+
+  for (const tool of requiredTools) {
+    try {
+      await new Deno.Command(tool, {
+        args: ["--version"], // the current needed tools all have this arg
+        stdout: "null",
+        stderr: "null",
+      })
+        .spawn()
+        .status;
+    } catch (e) {
+      if (e instanceof Deno.errors.NotFound) {
+        notInstalled.push(tool);
+      }
+    }
+  }
+  throw "Some required tools were not found: " + notInstalled;
 }

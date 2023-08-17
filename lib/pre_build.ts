6abb5c48830bb00bb8f939acc17d1ed7868f6ff8
David Sherret <dsherret@gmail.com>
2023-08-17 11:30:56 -0400

    Painful.

--- lib/pre_build.ts
+++ lib/pre_build.ts
@@ -271,7 +271,6 @@ function getAsyncLoaderText(
   useCache: boolean,
 ) {
   const exportNames = getExportNames(bindgenOutput);
-  console.log("Import meta url:", import.meta.url);
   const loaderUrl = import.meta.resolve("../loader.ts");
 
   let loaderText = `import { Loader } from "${loaderUrl}";\n`;

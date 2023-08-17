6955316321ef73119dc811c321582da21fd652c0
David Sherret <dsherret@gmail.com>
2023-08-17 11:10:06 -0400

    Update

--- lib/deps.ts
+++ lib/deps.ts
@@ -5,13 +5,11 @@ export * as base64 from "https://deno.land/std@0.198.0/encoding/base64.ts";
 export { parse as parseFlags } from "https://deno.land/std@0.198.0/flags/mod.ts";
 export { Sha1 } from "https://deno.land/std@0.142.0/hash/sha1.ts";
 export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
-export {
-  copy,
-  writeAll,
-} from "https://deno.land/std@0.144.0/streams/conversion.ts";
+export { copy } from "https://deno.land/std@0.198.0/streams/copy.ts";
+export { writeAll } from "https://deno.land/std@0.198.0/streams/write_all.ts";
 export { gunzip } from "https://deno.land/x/denoflate@1.2.1/mod.ts";
 export { Untar } from "https://deno.land/std@0.198.0/archive/untar.ts";
-export { Buffer } from "https://deno.land/std@0.144.0/io/mod.ts";
-export { emptyDir, ensureDir } from "https://deno.land/std@0.144.0/fs/mod.ts";
+export { Buffer } from "https://deno.land/std@0.198.0/io/mod.ts";
+export { emptyDir, ensureDir } from "https://deno.land/std@0.198.0/fs/mod.ts";
 export { expandGlob } from "https://deno.land/std@0.198.0/fs/expand_glob.ts";
 export { default as cacheDir } from "https://deno.land/x/dir@1.4.0/cache_dir/mod.ts";

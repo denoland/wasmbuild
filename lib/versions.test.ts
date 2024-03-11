26621be3749bbba6a752dc82accf43be87cc8db9
Asher Gomez <ashersaupingomez@gmail.com>
2024-03-12 07:48:03 +1100

    refactor: DRY up version logic

--- lib/versions.test.ts
+++ lib/versions.test.ts
@@ -1,32 +1,13 @@
 // Copyright 2018-2024 the Deno authors. MIT license.
 
 import { assertThrows } from "@std/assert/assert_throws";
-import { verifyVersions } from "./versions.ts";
-
-const correctMap: { [name: string]: string } = {
-  "wasm-bindgen": "0.2.91",
-  "wasm-bindgen-futures": "0.4.41",
-  "js-sys": "0.3.68",
-  "web-sys": "0.3.68",
-};
+import { verifyVersions, versions } from "./versions.ts";
 
 Deno.test("should verify when all correct", () => {
   const crate = {
     name: "test-crate",
-    getDependencyVersion(depName: string) {
-      return correctMap[depName];
-    },
-  };
-  verifyVersions(crate);
-});
-
-Deno.test("should verify when only wasm-bindgen version", () => {
-  const crate = {
-    name: "test-crate",
-    getDependencyVersion(depName: string) {
-      return {
-        "wasm-bindgen": correctMap["wasm-bindgen"],
-      }[depName];
+    getDependencyVersion(depName: keyof typeof versions) {
+      return versions[depName];
     },
   };
   verifyVersions(crate);
@@ -35,7 +16,7 @@ Deno.test("should verify when only wasm-bindgen version", () => {
 Deno.test("should error when no wasm-bindgen", () => {
   const crate = {
     name: "test-crate",
-    getDependencyVersion(_depName: string) {
+    getDependencyVersion(_depName: keyof typeof versions) {
       return undefined;
     },
   };
@@ -43,19 +24,19 @@ Deno.test("should error when no wasm-bindgen", () => {
     () => verifyVersions(crate),
     Error,
     `The crate 'test-crate' must have a dependency on wasm-bindgen ${
-      correctMap["wasm-bindgen"]
+      versions["wasm-bindgen"]
     } (found <NOT FOUND>).`,
   );
 });
 
 Deno.test("should error when has incorrect dependency", () => {
-  const crateNames = Object.keys(correctMap);
+  const crateNames = Object.keys(versions) as (keyof typeof versions)[];
   for (const crateName of crateNames) {
     const crate = {
       name: "test-crate",
       getDependencyVersion(depName: string) {
         return {
-          "wasm-bindgen": correctMap["wasm-bindgen"],
+          "wasm-bindgen": versions["wasm-bindgen"],
           [crateName]: "0.0.1",
         }[depName];
       },
@@ -64,7 +45,7 @@ Deno.test("should error when has incorrect dependency", () => {
       () => verifyVersions(crate),
       Error,
       `The crate 'test-crate' must have a dependency on ${crateName} ${
-        correctMap[crateName]
+        versions[crateName]
       } (found 0.0.1).`,
     );
   }

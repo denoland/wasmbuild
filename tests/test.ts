5b1c48defdcf946cff5d5e8d22c7bb0297377b28
David Sherret <dsherret@gmail.com>
2025-02-28 11:17:39 -0500

    Maybe fix

--- tests/test.ts
+++ tests/test.ts
@@ -3,13 +3,9 @@ import * as wasm from "./lib/deno_test.js";
 import * as wasm2 from "./lib_inline/deno_test.js";
 
 Deno.test("test works export", () => {
-  function hasStart() {
-    return Deno.readTextFileSync(import.meta.dirname + "/lib/deno_test.js")
-      .includes("__wbindgen_start");
-  }
   assertEquals(
     wasm.greet("Deno"),
-    hasStart() ? "Hello, Deno! Result: 4" : "Hello, Deno! Result: 3",
+    Deno.env.get("USES_START") ? "Hello, Deno! Result: 4" : "Hello, Deno! Result: 3",
   );
 });
 
